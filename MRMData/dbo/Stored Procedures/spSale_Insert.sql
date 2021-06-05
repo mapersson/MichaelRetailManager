7d56400f328ea3ad8447ba2d53e8a828e5e3f410
Michael Persson <62900241+mapersson@users.noreply.github.com>
2021-06-05 08:26:20 -0600

    Fix Identity Lookup
    Used Scope_Indentity now

--- MRMData/dbo/Stored Procedures/spSale_Insert.sql
+++ MRMData/dbo/Stored Procedures/spSale_Insert.sql
@@ -10,6 +10,6 @@ BEGIN
 	SET NOCOUNT ON
 	INSERT INTO dbo.Sale(CashierId, SaleDate, SubTotal, Tax, Total)
 	VALUES (@CashierId, @SaleDate, @SubTotal, @Tax, @Total);
-	SELECT @Id = @@IDENTITY;
+	SELECT @Id = SCOPE_IDENTITY();
 END
 
