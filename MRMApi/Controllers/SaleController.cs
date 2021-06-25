5273ce83d54ede0472b23954d57636f9c0c58ef8
Michael Persson <62900241+mapersson@users.noreply.github.com>
2021-06-25 10:37:38 -0600

    Added New API and Moved Over Functionality

---
+++ MRMApi/Controllers/SaleController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MRMDataManager.Library.DataAccess;
+using MRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MRMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SaleController : ControllerBase
+    {
+        [Authorize(Roles = "Cashier")]
+        public void Post(SaleModel sale)
+        {
+            var data = new SaleData();
+            var cashierId = User.FindFirst(ClaimTypes.NameIdentifier).ToString(); //RequestContext.Principal.Identity.GetUserId();
+
+            data.SaveSale(sale, cashierId);
+
+        }
+
+        [Authorize(Roles = "Admin,Manager")]
+        [Route("GetSalesReport")]
+        public List<SaleReportModel> GetSalesReport()
+        {
+            
+            if (User.IsInRole("Admin"))
+            {
+                //Add administrator activties
+            }
+
+            SaleData data = new SaleData();
+
+            return data.GetSaleReport();
+        }
+    }
+}
